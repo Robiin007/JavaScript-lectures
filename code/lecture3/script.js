ca9d90c5512c5ee6a8f7fbae70dc441464830275
Robiin007 <129576912+Robiin007@users.noreply.github.com>
2023-12-27 15:58:00 +0000

    lecture_3 is added

---
+++ code/lecture3/script.js
@@ -0,0 +1,49 @@
+// let string1 = 'hello robin';
+// let string2 = 'Hello robin';
+// let string2 = "Robin Singh \t SDE";
+// let string3 = `Your \n Software Engineer`;
+// // console.log(string1);
+//  console.log(string2);
+//  console.log(string3);
+// console.log(string1 + string2);
+// let str1 =  "Robin"+"singh";
+// let str2 = "Sandeep";
+// console.log(`${str1} is a friend of ${str2}`);
+// string1[0] = 'A';
+// console.log(string1);
+// console.log(string1 == string2);
+
+//let str1 = "hello I am Robin Singh";
+// console.log(str1.length);
+// console.log(str1.charAt(4));
+// console.log(str1.toLowerCase());
+// console.log(str1.toUpperCase());
+// console.log(str1.replace("hello", "hey"));
+// console.log(str1.concat(" from Geekster"));
+// console.log(str1.includes("Robin"));
+// console.log(str1.search("Robin"));
+// console.log(str1.slice(0, 2));
+// console.log(str1.substr(0, 5));
+//console.log(str1.split());
+// let b = false;
+// console.log(typeof String(b));
+//console.log(str1.startsWith("hello"));
+// let n = 5;
+// let string = "";
+
+// for (let i = 1; i <= n; i++) {
+//   // creating spaces
+//   for (let j = 1; j < i; j++) {
+//     string += " ";
+//   }
+
+//   for (let k = 1; k <= 2 * (n - i + 1) - 1; k++) {
+//     string += k;
+//   }
+//   string += "\n";
+// }
+// console.log(string);
+// let str = "Please Give Me rs 1000";
+// // let ans = Number.parseInt(str.slice(18));
+// console.log(str.toLowerCase());
+console.log("har\"".length);
