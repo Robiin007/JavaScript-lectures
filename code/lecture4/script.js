9816a9e21cdfa08fc09f4298812f5ea22ff104e1
Robiin007 <129576912+Robiin007@users.noreply.github.com>
2023-12-28 10:17:23 +0000

    lecture_4 is added

---
+++ code/lecture4/script.js
@@ -0,0 +1,66 @@
+// let student = {
+//     name: "Robin Singh",
+//     RollNo : 60,
+//     CN : "CU",
+//     Age: 25,
+//     198: "itisPresent"
+// }
+// console.log(student.name);
+// console.log(typeof student);
+// student.marks = 100;
+// console.log(student);
+// console.log(student["CN"]);
+// console.log(student["198"]);
+// console.log(delete student["198"]);
+// console.log(student);
+// for(let prop in student){
+//     console.log(prop);
+// }
+
+// let keys = Object.keys(student);
+// console.log(keys);
+
+// let keys = Object.getOwnPropertyNames(student);
+// console.log(keys);
+
+// let student = {
+//     aakash: "helloWorld",
+//     rn: 12,
+//     cn: 'DTU',
+//     address: {
+//         city: 'Mumbai',
+//         state:'Maharastra'
+//     }
+// }
+
+// console.log(student.address.state);
+
+// let company = {
+//     name : 'geekster',
+//     address: {
+//         street: '12 open end',
+//         city: 'Gurugram'
+//     },
+//     learning:{
+//         noOfEmployes: 10,
+//         boys: 7,
+//         girls: 4,
+//         hiring: 20,
+//         placement:{
+//             noOfEmployes:5,
+//             company: 'Amazon',
+//         Remainingstudents: 40,
+//         student:{
+//             name: "xyz",
+//             batchManager: "kjsd",
+//             EducatorName: 'Bitto'
+//         }
+//         }
+//     }
+// }
+
+// console.log(company.learning.placement.student.EducatorName);
+let obj = {
+    name: "Robin Singh"
+}
+console.log(JSON.stringify);
